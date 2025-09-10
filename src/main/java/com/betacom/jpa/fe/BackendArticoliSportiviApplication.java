71c6e0e3a4b95276b350b55782b677ab4010ac14
AlessandroVIV <alex01.vivaldi@gmail.com>
2025-09-10 15:19:01 +0200

    Initial commit

---
+++ src/main/java/com/betacom/jpa/fe/BackendArticoliSportiviApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.jpa.fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendArticoliSportiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendArticoliSportiviApplication.class, args);
+	}
+
+}
