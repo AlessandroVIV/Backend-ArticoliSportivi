97575b9c26cfc483bae1b8ae5ceb59015f0d87c4
Cristian44 <cristianborioni44@gmail.com>
2025-09-16 16:43:10 +0200

    aggiunto cross origin all'articolo controller

--- src/main/java/com/betacom/jpa/controllers/ArticoloController.java
+++ src/main/java/com/betacom/jpa/controllers/ArticoloController.java
@@ -1,6 +1,7 @@
 package com.betacom.jpa.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.betacom.jpa.response.ResponseBase;
 import com.betacom.jpa.response.ResponseList;
 import com.betacom.jpa.services.interfaces.IArticoloInterfaces;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/rest/articolo")
 public class ArticoloController {
